a9b49f5adbb3a32492bf0acaa9e25f5b8564588b
George Lahoud <george.lahoud2006@gmail.com>
2026-09-14 22:53:29 +1000

    Add football pitch canvas

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ root = tk.Tk()
 root.title("Football Lineup Builder")
 root.geometry("900x700")
 
+pitch = tk.Canvas(
+    root,
+    width=700,
+    height=600,
+    bg="green"
+)
+
+pitch.pack(pady=20)
+
 root.mainloop()
